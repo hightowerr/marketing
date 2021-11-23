e3fd138f428fc23d5edbb19a75db1fed81d75e59
OO-yink <yooglemisc@gmail.com>
2021-11-23 10:33:15 +0000

    blog post file

---
+++ resumetable.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# Function to produce more descriptive statistics.
+import pandas as pd
+from scipy import stats
+
+def resumetable(df):
+    print(f"Dataset Shape: {df.shape}")
+    summary = pd.DataFrame(df.dtypes,columns=['dtypes'])
+    summary = summary.reset_index()
+    summary['Name'] = summary['index']
+    summary = summary[['Name','dtypes']]
+    summary['Missing'] = df.isnull().sum().values
+    summary['NaN'] = df.isna().sum().values
+    summary['Uniques'] = df.nunique().values
+    summary['First Value'] = df.loc[0].values
+    summary['Second Value'] = df.loc[1].values
+    summary['Third Value'] = df.loc[2].values
+
+    for name in summary['Name'].value_counts().index:
+        summary.loc[summary['Name'] == name, 'Entropy'] = round(stats.entropy(df[name].value_counts(normalize=True), base=2),2) 
+
+    return summary
+
